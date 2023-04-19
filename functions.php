088fa95a2253230145a877c85ee2ec426656c37b
Edward Zota <zotaedward@gmail.com>
2023-04-19 18:18:28 +0200

    esercizio concluso + tutti i bonus e aggiunto css e sistemato la funzione

--- functions.php
+++ functions.php
@@ -2,29 +2,16 @@
 
 function createPassword($arrayAllCharacter,$lunghezzaPassword){
     $password=[];
-    if($_GET['Repeate'] == 'Si'){
-        for($i = 0 ; $i < $lunghezzaPassword; $i++){
-            $randomNumber=rand(0,count($arrayAllCharacter));
-    
-            foreach($arrayAllCharacter as $key => $character){
-                if($randomNumber==$key){
-                    $password[]=$character;
-                }
-            }
-        }
-    }elseif($_GET['Repeate'] == 'No'){
         $i = 0;
         while ($i <= $lunghezzaPassword) {
             $randomNumber=rand(0,count($arrayAllCharacter));
     
-            foreach($arrayAllCharacter as $key => $character){
-                if($randomNumber==$key && !in_array($randomNumber,$arrayAllCharacter)){
-                    $password[]=$character;
+                if(!in_array($arrayAllCharacter[$randomNumber],$password) || $_GET['Repeate'] == 'Si'){
+                    $password[]=$arrayAllCharacter[$randomNumber];
                     $i++;
                 }
-            }
+            
 
         }
-    }
     return $password;
 }
--- index.php
+++ index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password Generator</title>
-</head>
-<body>
-    <?php
+<?php
         require_once __DIR__ . './functions.php';
 
         if(isset($_GET['passLang'])){
@@ -35,43 +26,57 @@
         $allCharacter= $letter . $number . $symbol . $capitalLetter;
         $arrayAllCharacter=str_split($allCharacter);
 
+
         session_start();
-        if (isset($lunghezzaPassword)) {
+        if (isset($lunghezzaPassword) && $lunghezzaPassword>5) {
             $_SESSION['newPassword'] = $newPassword = createPassword($arrayAllCharacter,$lunghezzaPassword);
-            
         }
-    
+        if(isset($newPassword)){
+            header('Location: ./response.php');
+        }
     ?>
-    <div>
-        <?php
-            if(isset($newPassword)){
-                header('Location: ./response.php');
-            }
-        ?>
-    </div>
-    <form action="index.php" method="GET">
-        <div class="box">
-            <label for="passLang">Lunghezza Password:</label>
-            <input type="number" id="passLang" name="passLang">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h3 id="title">Generatore password random</h3>
+        <div id="control">
+            <?php
+                if($lunghezzaPassword<=5 && $lunghezzaPassword>0){
+                    ?><h3 id="alert">Richiesta una password maggiore di 5</h3><?php
+                }
+            ?>
         </div>
-        <div class="box">
-            <p>Accetti la ripetizione dei caratteri?</p>
-                <input type="radio" id="Si" name="Repeate" value="Si" checked>
-                <label for="Si" >Si</label><br>
-                <input type="radio" id="No" name="Repeate" value="No">
-                <label for="No">No</label><br>  
-        </div>
-        <div class="box">
-            <p>Che tipo di caratteri desideri includere:</p>
-                <input type="checkbox" id="Lettere" name="Lettere" value="Lettere" checked>
-                <label for="Lettere"> Lettere</label><br>
-                <input type="checkbox" id="Numeri" name="Numeri" value="Numeri" checked>
-                <label for="Numeri"> Numeri</label><br>
-                <input type="checkbox" id="Simboli" name="Simboli" value="Simboli" checked>
-                <label for="Simboli"> Simboli</label><br>
-        </div>
-        <button>Crea Password</button>
-    </form>
-    
+        <form action="index.php" method="GET">
+            <div class="box">
+                <label for="passLang">Lunghezza Password:</label>
+                <input type="number" id="passLang" name="passLang">
+            </div>
+            <div class="box">
+                <p>Accetti la ripetizione dei caratteri?</p>
+                    <input type="radio" id="Si" name="Repeate" value="Si" checked>
+                    <label for="Si" >Si</label><br>
+                    <input type="radio" id="No" name="Repeate" value="No">
+                    <label for="No">No</label><br>  
+            </div>
+            <div class="box">
+                <p>Che tipo di caratteri desideri includere:</p>
+                    <input type="checkbox" id="Lettere" name="Lettere" value="Lettere" checked>
+                    <label for="Lettere"> Lettere</label><br>
+                    <input type="checkbox" id="Numeri" name="Numeri" value="Numeri" checked>
+                    <label for="Numeri"> Numeri</label><br>
+                    <input type="checkbox" id="Simboli" name="Simboli" value="Simboli" checked>
+                    <label for="Simboli"> Simboli</label><br>
+            </div>
+            <button>Crea Password</button>
+        </form>
+    </main>
 </body>
 </html>
